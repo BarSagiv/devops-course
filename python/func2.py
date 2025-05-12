a8bb5b690ea4cd4dcd927a64ec4c07f430cfb6bb
Bar Sagiv <barbor161@gmail.com>
2025-05-12 21:07:03 +0300

    adding funcs python

--- python/func2.py
+++ python/func2.py
@@ -1,8 +1,4 @@
-def list_sum(list):
-    return sum(list)
-list = [ 1, 1, 2, 3, 4, 5 ]
-# write a funtion that tests if a list of strings contains the same string twich
-
+# This is a funtion that tests if two lists of strings contains the same string twich, one is true and one is false.
 def test_list(list):
     for i in range(len(list)):
         for j in range(i+1, len(list)):
@@ -10,6 +6,9 @@ def test_list(list):
                 return True
     return False
 
-# test the function
+# Test the function
 list = [ 'a', 'b', 'c', 'd', 'e', 'f' ]
 print(test_list(list))
+
+list2 = [ 'a', 'b', 'c', 'd', 'e', 'f', 'a' ]
+print(test_list(list2))
